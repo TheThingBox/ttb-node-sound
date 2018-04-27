6639bd12066959deda79cab669f6e4b27f1f5439
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-04-27 16:46:10 +0200

    init

---
+++ sound.js
@@ -0,0 +1,94 @@
+module.exports = function(RED) {
+  "use strict";
+  var Player = require("player");
+  var path = require("path")
+
+  var VOLUME_INCREMENT_STEP = 5/100;
+
+  function Sound(n) {
+    RED.nodes.createNode(this, n);
+
+    this.sound = n.sound;
+    this.playing = false;
+    createPlayer(this, this.sound);
+
+    var node = this;
+    node.messageOnFinish = {};
+
+    this.on('input', function(msg) {
+      if(msg.intent || msg.intent == 0) {
+        if(msg.intent == 1) { // open
+          startPlayer(node, msg);
+          msg.payload = n.name;
+          node.send([msg, null]);
+        } else if(msg.intent == 0) { // close
+          stopPlayer(node);
+          node.send([msg,null]);
+        } else if(msg.intent == 2) { // more
+          var vol = node.player.getVolume();
+          node.player.setVolume(vol + VOLUME_INCREMENT_STEP);
+        } else if(msg.intent == 3) { // less
+          var vol = node.player.getVolume();
+          node.player.setVolume(vol - VOLUME_INCREMENT_STEP);
+        }
+      } else if(msg.intensity) {
+        node.player.setVolume(Number(msg.intensity)/100);
+      } else if(msg.command && msg.command === "stop" && node.playing) {
+        stopPlayer(node);
+        node.send([msg,null]);
+      } else {
+        startPlayer(node, msg);
+        node.send([msg, null]);
+      }
+    });
+
+    this.on("close", function() {
+      if(node.playing)
+        node.player.stop();
+    });
+  }
+  RED.nodes.registerType("sound", Sound);
+
+  function createPlayer(node, sound) {
+    stopPlayer(node);
+    node.player = new Player([sound],{downloads:"/root/userdir/"});
+    node.player.on("error", function(err) {
+      node.warn(err);
+    });
+    node.player.on("finish", function(current) {
+      node.send([null, node.messageOnFinish]);
+      node.playing = false;
+    });
+  }
+
+  function stopPlayer(node) {
+    if(!node.playing) return;
+    node.player.stop();
+    node.playing = false;
+  }
+
+  function startPlayer(node, msg) {
+    var toplay =  msg.sound || msg.value || msg.payload || "";
+
+    if(toplay == "" || typeof toplay != "string")
+      toplay = node.sound;
+
+    if(toplay.charAt(0) != "/" && toplay.indexOf("http://") != 0)
+      toplay = path.join('/root', 'sounds', toplay)
+
+    if(toplay.indexOf(".") == -1)
+      toplay += ".mp3";
+
+    if(toplay && node.player.list[0] != toplay && toplay != "/root/sounds/.mp3") {
+      createPlayer(node, toplay);
+    }
+
+    if(node.playing){
+      stopPlayer(node);
+    }
+
+    node.player.play();
+    node.playing = true;
+    node.messageOnFinish = msg;
+  }
+}
